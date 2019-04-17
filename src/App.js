450ce3faddd7bb360468d013eb5f9ce206df8f6b
Brenda <brenda@brenda.com>
2019-04-17 12:42:12 +0200

    Simpsons not simpons

--- src/App.js
+++ src/App.js
@@ -5,15 +5,37 @@ import "./App.css";
 import Quote from "./Quote";
 import Quotes from "./Quotes";
 
+
+const homer = {
+  quote:
+    "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+  character: "Homer Simpson",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+};
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+
   render() {
+    const x = this.state.working ? 'App-logo-on': 'App-logo-off';
+    const y = () => {
+      this.setState({working: !this.state.working});
+    };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={x} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Quotes/>
+
+        <button onClick={y} style={{fontSize:"200%"}}>Click me</button>
+        <Quote {...homer}/>
       </div>
     );
   }
